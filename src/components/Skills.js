5482325bd7dbda7cb57d866169adb71a5a67a465
Anthony-Am2003 <tonyaamb2003@gmail.com>
2023-08-14 21:51:28 -0500

    Skills update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -42,15 +42,15 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Backend Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Frontend Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Data Bases</h5>
                             </div>
                         </Carousel>
                     </div>
