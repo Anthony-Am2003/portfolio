7ef36ffd1a8c742e2d2dd3023b37abf0c23ef77f
Anthony-Am2003 <tonyaamb2003@gmail.com>
2023-08-16 21:57:42 -0500

    new component tech finish

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <Technologies/>
       <Skills />
+      <Technologies/>
       <Projects />
       <Contact />
       <Footer />
--- src/components/Tech.js
+++ src/components/Tech.js
@@ -13,30 +13,57 @@ import sequelizeIcon from "../assets/img/sequelize.png";
 import pythonIcon from "../assets/img/python.svg";
 import fastapiIcon from "../assets/img/fastApi.png";
 import dockerIcon from "../assets/img/docker.svg";
+import Carousel from 'react-multi-carousel';
 
 
 export const Technologies = () => {
-  return (
-    <section className="technologies" id="technologies">
-      <div className="container">
-        <h2>Technologies</h2>
-        <div className="technology-icons">
-          <img src={htmlIcon} alt="HTML" />
-          <img src={cssIcon} alt="CSS" />
-          <img src={jsIcon} alt="JavaScript" />
-          <img src={reactIcon} alt="React" />
-          <img src={reduxIcon} alt="Redux" />
-          <img src={nodejsIcon} alt="Node.js" />
-          <img src={expressIcon} alt="Express" />
-          <img src={gitIcon} alt="Git" />
-          <img src={githubIcon} alt="GitHub" />
-          <img src={postgresqlIcon} alt="PostgreSQL" />
-          <img src={sequelizeIcon} alt="Sequelize" />
-          <img src={pythonIcon} alt="Python" />
-          <img src={fastapiIcon} alt="FastAPI" />
-          <img src={dockerIcon} alt="Docker" />
+    const responsive = {
+      superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+      },
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+      }
+    };
+  
+    return (
+      <section className="technologies" id="technologies">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="technology-bx wow zoomIn">
+                <h2>Technologies</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br /> Lorem Ipsum has been the industry's standard dummy text.</p>
+                <div className="technology-icons">
+                    <img src={htmlIcon} alt="HTML" />
+                    <img src={cssIcon} alt="CSS" />
+                    <img src={jsIcon} alt="JavaScript" />
+                    <img src={reactIcon} alt="React" />
+                    <img src={reduxIcon} alt="Redux" />
+                    <img src={nodejsIcon} alt="Node.js" />
+                    <img src={expressIcon} alt="Express" />
+                    <img src={gitIcon} alt="Git" />
+                    <img src={githubIcon} alt="GitHub" />
+                    <img src={postgresqlIcon} alt="PostgreSQL" />
+                    <img src={sequelizeIcon} alt="Sequelize" />
+                    <img src={pythonIcon} alt="Python" />
+                    <img src={fastapiIcon} alt="FastAPI" />
+                    <img src={dockerIcon} alt="Docker" />
         </div>
-      </div>
-    </section>
-  );
-};
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
